2384fa4ceff911ee6f5956d4b73eb0e28b0bbfc4
anthony-guillot <guillot.anthony01@gmail.com>
2021-10-15 10:05:54 +0200

    tablelexcio

--- tableLexico.c
+++ tableLexico.c
@@ -1,36 +1,125 @@
 #include "tableLexico.h"
 #include <string.h>
 
-void initTableLexico(lexico* table){
-    for(int i=0;i<5000;i++){
-        table[i].longueur=0;
-        table[i].lexeme="";
-        table[i].suivant=0;
+lexico table[500];
+hashc tabhashcode[31];
+/*---Calcul le hashcode d'un lexeme---*/
+int calcul_hascode(char *mot, int taille)
+{
+    int somme;
+    for (int i = 0; i < taille; i++)
+    {
+        somme += mot[i];
+    }
+    return somme % 32;
+}
+
+/*---Initialise le tableau de hashcode et le tableau lexico---*/
+void init(lexico *table, hashc *tablehc)
+{
+    for (int i = 0; i < 500; i++)
+    {
+        table[i].longueur = 0;
+        table[i].lexeme = "";
+        table[i].suivant = -1;
+    }
+    for (int i = 0; i < 32; i++)
+    {
+        tablehc[i].hashcode = -1;
     }
 }
 
-void afficherTableLexico(lexico* table){
-    int i=0;
-    for(i=0;table[i].longueur!=0;i++){
-        printf("%d | %s | %d\n",table[i].longueur,table[i].lexeme,table[i].suivant);
+/*---Affichage du tableau lexicographique---*/
+void afficherTableLexico(lexico *table)
+{
+    int i = 0;
+    for (i = 0; table[i].longueur != 0; i++)
+    {
+        printf("%d | %s | %d\n", table[i].longueur, table[i].lexeme, table[i].suivant);
     }
 }
 
-int inserer(char * lexeme,lexico* table){
-    int i=0;
-    for(i=0;table[i].longueur!=0;i++){
-        if(strcmp(lexeme,table[i].lexeme)==0){
-            return i;
+/*---Parcours le tableau, et insere le lexeme, sa taille a la fin et retourne la position inserer---*/
+int insererbis(char *lexeme, lexico *table)
+{
+    int i;
+    for (i = 0; i < 500; i++)
+    {
+        if (table[i].longueur == 0)
+        {
+            table[i].longueur = strlen(lexeme);
+            table[i].lexeme = lexeme;
+            break;
         }
     }
-    table[i].longueur=strlen(lexeme);
-    table[i].lexeme=lexeme;
-    table[i].suivant=-1;
-    return i;
+    return i - 1;
 }
 
-int main(){
-    lexico* table;
-    initTableLexico(table);
+/*---Verifier si le lexeme a position donnée est different , si oui, il verifie le suivant s'il en existe un---*/
+void verif_lexeme(char *lexeme, lexico *table, int position)
+{
+    int longeur;
+    longeur = strlen(lexeme);
+    if (longeur != table[position].longueur)
+    {
+        if (table[position].suivant == -1)
+        {
+            table[position].suivant = insererbis(lexeme, table);
+        }
+        else
+        {
+            verif_lexeme(lexeme, table, table[position].suivant);
+        }
+    }
+    else
+    {
+        if (strcmp(lexeme, table[position].lexeme) == 0)
+        {
+            return;
+        }
+        else
+        {
+            if (table[position].suivant == -1)
+            {
+                table[position].suivant = insererbis(lexeme, table);
+            }
+            else
+            {
+                verif_lexeme(lexeme, table, table[position].suivant);
+            }
+        }
+    }
+}
 
+/*---Verifie le hashcode pour savoir ou commencer a parcourir les verifications---*/
+void inserer(char *lexeme, lexico *table, hashc *tablehc)
+{
+    int retour = verif_hachcode(lexeme, tablehc);
+    if (retour == -1)
+    {
+        tablehc[calcul_hascode(lexeme, strlen(lexeme))].hashcode = insererbis(lexeme, table);
+    }
+    else
+    {
+        verif_lexeme(lexeme, table, retour);
+    }
+}
+
+/*---Verifie si le haschode est deja present, et si oui, renvoie sa poosition---*/
+int verif_hachcode(char *lexeme, hashc *tabhc)
+{
+    int hc;
+    hc = calcul_hascode(lexeme, strlen(lexeme));
+    if (tabhc[hc].hashcode == -1)
+    {
+        return -1;
+    }
+    else
+    {
+        return tabhc[hc].hashcode;
+    }
+}
+int main()
+{
+    init(table, tabhashcode);
 }
--- tableLexico.h
+++ tableLexico.h
@@ -1,5 +1,11 @@
+#ifndef TABLE_LEXICO_H
+#define TABLE_LEXICO_H
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct{
+    int hashcode;
+}hashc;
 
 typedef struct
 {
@@ -7,3 +13,12 @@ typedef struct
     char* lexeme;
     int suivant;
 }lexico;
+int calcul_hascode(char* mot,int taille);
+void init(lexico* table,hashc* tablehc);
+void verif_lexeme(char * lexeme,lexico* table,int position);
+void afficherTableLexico(lexico* table);
+int insererbis(char * lexeme,lexico* table);
+void verif_lexeme(char * lexeme,lexico* table,int position);
+void inserer(char * lexeme,lexico* table,hashc* tablehc);
+int verif_hachcode(char *lexeme,hashc *tabhc);
+#endif
