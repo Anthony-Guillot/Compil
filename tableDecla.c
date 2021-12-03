cfa47a1713b61c06913e12fed10a4d20dd8914cc
SheaneOniridia <76440949+SheaneOniridia@users.noreply.github.com>
2021-12-03 14:39:48 +0100

    Add files via upload
    Changements sur la fonction, commentaires utile a la manière d'utiliser, incomplet encore

--- tableDecla.c
+++ tableDecla.c
@@ -13,21 +13,39 @@ void initDecla(){
     tabledecla[3][i] = -1; 
     tabledecla[4][i] = -1;
   }
-}
 
-void AjoutTD(int i, int Nature, int Region){
-  if(tabledecla[0][i]==-1){
-    tabledecla[0][i]=Nature;
-    tabledecla[2][i]=Region;
-  }
+    tabledecla[0][0] = 3; 
+    tabledecla[2][0] = 0;
+    tabledecla[4][0] = 1;
+
+     tabledecla[0][0] =3; 
+    tabledecla[2][0] = 0;
+    tabledecla[4][0] = 1;
+
+     tabledecla[0][0] = 3; 
+    tabledecla[2][0] = 0;
+    tabledecla[4][0] = 1;
+
+     tabledecla[3][0] = 3; 
+    tabledecla[2][0] = 0;
+    tabledecla[4][0] = 1;
+  
+}
+/*---Ajoute dans la table de déclaration les éléments---*/
+/*lexassocie et typeassocie vienne de base par association_nom dans le YACC*/
+/*---si non var/par alors typeassocie =-1 et desc !=-1---*/
+void AjoutTD(int Nature, int Region, int lexassocie, int typeassocie, int desc, int exec){
+  if(tabledecla[0][lexassocie]==-1){
+    tabledecla[0][lexassocie]=Nature;
+    tabledecla[2][lexassocie]=Region;
   else{
-    if(tabledecla[1][i]==-1){
-      tabledecla[1][i]==debordement_libre;
-      AjoutTD(debordement_libre, Nature, Region);
+    if(tabledecla[1][lexassocie]==-1){
+      tabledecla[1][lexassocie]==debordement_libre;
+      AjoutTD();
       debordement_libre++;
     }
     else{
-      AjoutTD(tabledecla[1][i], Nature, Region);
+      AjoutTD();
     }
   }
 }
@@ -37,9 +55,9 @@ void AfficherTD(){
   printf("Table primaire 0 à 500\n");
   for (i=0; i<500; i++){
     if(i%50 == 0){
-      printf("   Index    Nature    Suivant     N°Region     Index     Exec\n ");
-    }
-    printf("     %d        %d          %d          %d           %d           %d\n", i, tabledecla[0][i], tabledecla[1][i], tabledecla[2][i], tabledecla[3][i], tabledecla[4][i]);
+      printf("\tIndex\tNature\tSuivant\tN°Region\tIndex\tExec\n ");
+    }0
+    printf("\t%d \t%d \t%d \t%d \t%d \t%d \n", i, tabledecla[0][i], tabledecla[1][i], tabledecla[2][i], tabledecla[3][i], tabledecla[4][i]);
   }
 
    printf("\n\nTable débordement\n");
