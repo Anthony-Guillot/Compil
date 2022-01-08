bafaf41ebce3f897155e9b6d4e2d10094129af63
SheaneOniridia <76440949+SheaneOniridia@users.noreply.github.com>
2022-01-08 15:41:04 +0100

    Add files via upload
    Version1.0 de la tableDecla

--- tableDecla.c
+++ tableDecla.c
@@ -7,47 +7,60 @@ int debordement_libre = 500;
 void initDecla(){
   int i;
   for (i=0; i<1000; i++){
-    tabledecla[0][i] = -1; 
+    tabledecla[0][i] = -1;
     tabledecla[1][i] = -1;
     tabledecla[2][i] = -1;
-    tabledecla[3][i] = -1; 
+    tabledecla[3][i] = -1;
     tabledecla[4][i] = -1;
   }
 
-    tabledecla[0][0] = 3; 
+    tabledecla[0][0] = 3;
     tabledecla[2][0] = 0;
     tabledecla[4][0] = 1;
 
-     tabledecla[0][0] =3; 
+     tabledecla[0][0] =3;
     tabledecla[2][0] = 0;
     tabledecla[4][0] = 1;
 
-     tabledecla[0][0] = 3; 
+     tabledecla[0][0] = 3;
     tabledecla[2][0] = 0;
     tabledecla[4][0] = 1;
 
-     tabledecla[3][0] = 3; 
+     tabledecla[3][0] = 3;
     tabledecla[2][0] = 0;
     tabledecla[4][0] = 1;
-  
+
 }
 /*---Ajoute dans la table de déclaration les éléments---*/
-/*lexassocie et typeassocie vienne de base par association_nom dans le YACC*/
-/*---si non var/par alors typeassocie =-1 et desc !=-1---*/
-void AjoutTD(int Nature, int Region, int lexassocie, int typeassocie, int desc, int exec){
+/*lexassocie viens de base par association_nom dans le YACC = son indice*/
+/*---Desc = autre indice contenant sa description---*/
+void AjoutTD(int Nature, int Region, int lexassocie, int desc, int exec){
+
   if(tabledecla[0][lexassocie]==-1){
     tabledecla[0][lexassocie]=Nature;
     tabledecla[2][lexassocie]=Region;
+    tabledecla[3][lexassocie]=desc;
+    tabledecla[4][lexassocie]=exec;
   else{
     if(tabledecla[1][lexassocie]==-1){
       tabledecla[1][lexassocie]==debordement_libre;
-      AjoutTD();
+      AjoutTD(Nature, Region debordement_libre, desc, exec);
       debordement_libre++;
     }
     else{
-      AjoutTD();
+      AjoutTD(Nature, Region, tabledecla[1][lexassocie], desc, exec);
     }
   }
+}
+
+/*---Recupére la description---*/
+int DescClass(int indice){
+  return tabledecla[3][indice];
+}
+
+/*---Récupere l'execution, permet de calculer l'execution avec DescClass---*/
+int ExecClass(int desc){
+  return tabledecla[4][indice];
 }
 
 void AfficherTD(){
